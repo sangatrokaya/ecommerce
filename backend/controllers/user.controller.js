194e7ad526d1e797dc55b8173a73134932a6bdab
Sangat Rokaya <rokayasangat123@gmail.com>
2024-11-26 22:17:53 +0545

    integrate: bcryptjs for password hashing

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,15 +1,18 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 const signUp = async (req, res, next) => {
   try {
-    let { email } = req.body;
+    let { email, password } = req.body;
     let userExists = await User.findOne({ email });
     if (userExists) {
       let err = new Error(`User with email ${email} already exists!`);
       err.status = 400; // 400 refers to bad request
       throw err;
     }
-    let user = await User.create(req.body);
+    let salt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(password, salt);
+    let user = await User.create({ ...req.body, password: hashedPassword });
     res.send({
       message: "User Registered Successfully!",
       user: {
