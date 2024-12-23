d572a0a662bda16878285d3af9ad636c6c051cad
Sangat Rokaya <rokayasangat123@gmail.com>
2024-12-23 20:42:56 +0545

    add: product model

---
+++ backend/models/product.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+  name: String,
+  comments: String,
+  rating: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: String,
+    description: String,
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      //   enum: ["Electronics", "Clothing", "Stationery"]
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+    },
+    rating: Number,
+    numReviews: Number,
+    reviews: [reviewSchema],
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
