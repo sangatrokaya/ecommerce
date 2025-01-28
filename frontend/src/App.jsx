1c64f5dd830b230a20f8aff8e32ced9d576aaa9a
Sangat Rokaya <rokayasangat123@gmail.com>
2025-01-28 15:22:06 +0545

    add: productpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,17 @@ import Footer from "./components/Footer";
 import { Col, Container, Row } from "react-bootstrap";
 import Product from "./components/Product";
 import products from "./product";
+import ProductPage from "./pages/ProductPage";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Container>
+      <Container className="my-3">
+        {/* <ProductPage /> */}
         <Row>
           {products.map((product) => (
-            <Col sm={12} md={6} lg={4} xlg={3}>
+            <Col sm={12} md={6} lg={4} xlg={3} key={product.name}>
               <Product product={product} />
             </Col>
           ))}
---
+++ frontend/src/pages/ProductPage.jsx
@@ -0,0 +1,71 @@
+import { Button, Col, Image, ListGroup, Row } from "react-bootstrap";
+import Rating from "../components/Rating";
+
+const ProductPage = () => {
+  let product = {
+    name: "iPhone 13 Pro 256GB Memory",
+    image: "/images/phone.jpg",
+    description:
+      "Introducing the iPhone 13 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
+    brand: "Apple",
+    category: "Electronics",
+    price: 599.99,
+    countInStock: 8,
+    rating: 4.0,
+    numReviews: 8,
+  };
+  return (
+    <>
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} fluid />
+        </Col>
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>${product.price}</strong>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={product.rating} text={product.numReviews} />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span>{product.description}</span>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <ListGroup>
+            <ListGroup.Item>
+              <Row>
+                <Col>Price</Col>
+                <Col>
+                  <strong>{product.price}</strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>Status</Col>
+                <Col>
+                  <strong>
+                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                  </strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button variant="secondary" disabled={product.countInStock == 0}>
+                Add to Cart
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductPage;
