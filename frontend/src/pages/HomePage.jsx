e7240f33c0ceccdf2787635277ef357911cd572e
Sangat Rokaya <rokayasangat123@gmail.com>
2025-01-30 12:14:30 +0545

    use: fetch api to fetch products from db

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,17 @@
 import { Col, Row } from "react-bootstrap";
-import products from "../product";
 import Product from "../components/Product";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("api/v1/products")
+      .then((resp) => resp.json())
+      .then((data) => setProducts(data))
+      .catch((err) =>
+        console.log("Error occurred while fetching api!", err.message)
+      );
+  }, []);
   return (
     <>
       <h1>Latest Products</h1>
