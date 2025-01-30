165ee0736544b5a0512df7acd6f5b311c9205d83
Sangat Rokaya <rokayasangat123@gmail.com>
2025-01-30 11:45:54 +0545

    update: alternative way of setting react routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
@@ -8,6 +13,7 @@ import ProductPage from "./pages/ProductPage.jsx";
 import CartPage from "./pages/CartPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 
+/* // Setting up react routes
 const router = createBrowserRouter([
   {
     path: "/",
@@ -31,7 +37,19 @@ const router = createBrowserRouter([
       },
     ],
   },
-]);
+]); */
+
+// ALternative way to setting up react routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<HomePage />} />
+      <Route path="product" element={<ProductPage />} />
+      <Route path="cart" element={<CartPage />} />
+      <Route path="signin" element={<LoginPage />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
