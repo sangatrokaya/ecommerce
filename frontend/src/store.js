0062a806a9e0d606426df52118ef5a2fe1015e53
Sangat Rokaya <rokayasangat123@gmail.com>
2025-02-04 11:57:30 +0545

    add: apiSlice base to use rtk-query features

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
+import { apiSlice } from "./slices/apiSlice";
 
 export const store = configureStore({
   reducer: {
+    // Add the generated reducer as a specific top-level slice
+    [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartReducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware().concat(apiSlice.middleware);
   },
 });
