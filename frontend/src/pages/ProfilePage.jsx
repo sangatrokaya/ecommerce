cb800d4f8aa81d990f1f9cebbe289f03177affba
Sangat Rokaya <rokayasangat123@gmail.com>
2025-02-19 12:04:23 +0545

    add: My orders section design in profilepage

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import { Row, Col, Form, Button } from "react-bootstrap";
+import { Row, Col, Form, Button, Table } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { useUpdateUserProfileMutation } from "../slices/userApiSlice";
 import { toast } from "react-toastify";
 import { setCredentials } from "../slices/authSlice";
 import { useGetMyOrdersQuery } from "../slices/orderSlice";
+import Message from "../components/Message";
+import { FaTimes } from "react-icons/fa";
 
 const ProfilePage = () => {
   const [name, setName] = useState("");
@@ -85,6 +87,52 @@ const ProfilePage = () => {
       </Col>
       <Col md={8}>
         <h3>My Orders</h3>
+        {ordersLoading ? (
+          <h1>Loading...</h1>
+        ) : error ? (
+          <Message variant="danger">{error.data.error}</Message>
+        ) : (
+          <Table responsive hover striped className="table-sm">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Date</th>
+                <th>Total</th>
+                <th>Paid</th>
+                <th>Delivered</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>${order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.deliveredAt.substring(0, 10)
+                    ) : (
+                      <FaTimes style={{ color: "red" }} />
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      order.deliveredAt.substring(0, 10)
+                    ) : (
+                      <FaTimes style={{ color: "red" }} />
+                    )}
+                  </td>
+                  <td>
+                    <Button variant="light" size="sm">
+                      Details
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
       </Col>
     </Row>
   );
