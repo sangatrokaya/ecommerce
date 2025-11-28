29a3ae37aced99906eb505f800cc02488666a178
Sangat Rokaya <rokayasangat123@gmail.com>
2025-11-28 14:49:05 +0545

    add: UI for user register page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,7 +1,48 @@
-import React from "react";
-
+import FormContainer from "../components/FormContainer";
+import { Card, FormGroup, Form, Button, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <FormContainer>
+      <Card
+        className="shadow p-4 rounded"
+        style={{ margin: "auto", maxWidth: "400px" }}
+      >
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          <Form>
+            <FormGroup controlId="email" className="my-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                // value={email}
+              ></Form.Control>
+            </FormGroup>
+            <FormGroup controlId="password" className="my-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                // value={password}
+              ></Form.Control>
+            </FormGroup>
+            <Button type="submit" variant="primary" className="w-100 mt-2">
+              Sign Up
+            </Button>
+          </Form>
+          <Row className="py-3 text-center">
+            <Col>
+              Already Registered?
+              <Link to="/signin" className="text-decoration-none">
+                Log In
+              </Link>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </FormContainer>
+  );
 };
 
 export default RegisterPage;
